0a25f5df440a71d47fb3721b0c71a7022e9ded85
Shirene Boyd <shirenekadkhodai@gmail.com>
2023-03-16 13:30:19 -0500

    added a useEffect that takes care of the local storage side effect for checking whether a user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,29 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  
+
+//control local storage to not have an infinite loop
+//runs once when app starts up
+useEffect(() => {
+//check for stored user logged in info
+const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+if (storedUserLoggedInInformation === '1'){
+  setIsLoggedIn(true);
+}
+}, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');// 0 = no, 1 = yes to being logged in
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    //clear the local storage automatically
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
